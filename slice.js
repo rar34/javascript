424b7e68dfffa8ac52cbb3be86d59d54875db25b
rar34 <ra.cse34@gmail.com>
2025-07-25 12:22:06 +0600

    added slice.js file and implemented search functionality using spit and includes from a string

---
+++ slice.js
@@ -0,0 +1,10 @@
+const sentence = "I am a good working person";
+
+const wordArray = sentence.split(' ');
+
+ console.log(wordArray)
+searchKey = 'Good'
+
+ search = wordArray.includes(searchKey.toLowerCase());
+
+ console.log(search)
