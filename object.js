8236d1c4c5d1d032d50d3ac92b79f5c202403d5a
rar34 <ra.cse34@gmail.com>
2025-07-26 11:19:31 +0600

    added object.js file to solve object related problem

---
+++ object.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Abul Hossain",
+    age: 18,
+    roll: 15,
+    section: "Science",
+    Address: "Mymensingh"
+}
+
+
+
+console.log(student.name)
+console.log(student["section"])
