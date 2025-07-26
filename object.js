8a6e80bba1466e1a7e8b5b88bacb15a33e960851
rar34 <ra.cse34@gmail.com>
2025-07-26 11:35:42 +0600

    get value from object using for in loop

--- object.js
+++ object.js
@@ -8,5 +8,14 @@ const student = {
 
 
 
-console.log(student.name)
-console.log(student["section"])
+// console.log(student.name)
+// console.log(student["section"])
+
+// const key = Object.keys(student)
+
+// console.log(key)
+
+for( const prop in student){
+    // console.log(prop)
+    console.log(prop + ":" + student[prop])
+}
