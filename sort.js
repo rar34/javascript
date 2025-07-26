ae93fb3f2cf39beec5ea18feef748d058c17ff3c
rar34 <ra.cse34@gmail.com>
2025-07-26 12:04:44 +0600

    sorted numbers in ascending order

--- sort.js
+++ sort.js
@@ -1,3 +1,11 @@
-const persons =['dakib', 'sakib', 'makib', 'Akib', 'pakib'];
+const persons = ['dakib', 'sakib', 'makib', 'Akib', 'pakib'];
 const sortedPerson = persons.sort();
-console.log(sortedPerson)
+// console.log(sortedPerson)
+
+const numbers = [21, 12, 25, 2, 4, 62, 42, 15];
+
+const revAs = numbers.sort((a, b) => a - b);
+// const revDs = numbers.sort((a, b) => b - a);
+
+console.log(revAs)
+// console.log(revDs)
