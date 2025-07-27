96baff166d106a276d2fef6fda8eff822457ad56
rar34 <ra.cse34@gmail.com>
2025-07-27 17:15:09 +0600

    add all numbers from an array using function return

--- function.js
+++ function.js
@@ -1,6 +1,11 @@
-function Add(a,b){
-return a + b;
+function Add(numbers) {
+    let sum = 0;
+    for (num of numbers) {
+        sum = sum + num;
+    }
+    return sum;
 }
 
-const sum =  Add(10,20);
+const numbs = [54, 25, 45, 62, 5, 51];
+const sum = Add(numbs);
 console.log(sum)
