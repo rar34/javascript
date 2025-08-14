f3eaa69472269c38f00a861d9334ea63b433956c
rar34 <ra.cse34@gmail.com>
2025-08-14 22:59:52 +0600

    make comment box and post comment the webpage

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,5 @@
 const foodContainer = document.getElementById('food-Container');
-const h1 =  document.createElement('h1');
+const h1 = document.createElement('h1');
 h1.innerText = 'My Favorite food';
 
 
@@ -19,14 +19,23 @@ ul.appendChild(li3)
 
 foodContainer.appendChild(ul)
 
-const makeGreen =  document.getElementById('make-green');
-makeGreen.addEventListener('click', function(){
-    document.body.style.backgroundColor='green';
+const makeGreen = document.getElementById('make-green');
+makeGreen.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
 })
 
 const white = document.getElementById('make-white');
 white.onclick = makeWhite;
 
-function makeWhite(){
-    document.body.style.backgroundColor="white"
+function makeWhite() {
+    document.body.style.backgroundColor = "white"
 }
+
+const textChange = document.getElementById('change-text');
+function makeChange() {
+    textChange.innerText = 'Hello JS'
+}
+
+document.getElementById('handleListener').addEventListener('click', function(){
+    textChange.innerText = 'Text changed by handle event listener'
+})
