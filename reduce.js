492e3802fd5411937320e2e327a5f9957382b683
rar34 <ra.cse34@gmail.com>
2025-08-26 22:20:59 +0600

    calculate total price from an array

--- reduce.js
+++ reduce.js
@@ -2,4 +2,13 @@ const numbers = [10, 21, 5, 3, 1, 22]
 
 const total = numbers.reduce((a, b) => a + b, 0)
 
-console.log(total);
+const products = [
+    { id: 1, name: 'lenovo', price: 65000 },
+    { id: 1, name: 'Hp', price: 50000 },
+    { id: 1, name: 'Dell', price: 45000 },
+    { id: 1, name: 'Mac', price: 154000 }
+]
+
+const productPrice = products.map(p => p.price)
+const totalPrice = productPrice.reduce((a, b) => a + b, 0)
+console.log('Total price is ', totalPrice);
